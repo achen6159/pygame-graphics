5ba4fe719934d5614981cb244e3f51065fc12095
achen6159 <31477651+achen6159@users.noreply.github.com>
2018-01-04 09:33:36 -0500

    Add files via upload

--- graphics_demo.py
+++ graphics_demo.py
@@ -31,6 +31,13 @@ BRIGHT_GREEN = (70, 244, 39)
 YELLOW = (244, 234, 41)
     
 
+def draw_cloud(x, y):
+    pygame.draw.ellipse(screen, WHITE, [x, y + 20, 40 , 40])
+    pygame.draw.ellipse(screen, WHITE, [x + 60, y + 20, 40 , 40])
+    pygame.draw.ellipse(screen, WHITE, [x + 20, y + 10, 25, 25])
+    pygame.draw.ellipse(screen, WHITE, [x + 35, y, 50, 50])
+    pygame.draw.rect(screen, WHITE, [x + 20, y + 20, 60, 40])
+    
 # Game loop
 done = False
 
@@ -46,8 +53,10 @@ while not done:
     ''' leave this section alone for now ''' 
 
     # Drawing code (Describe the picture. It isn't actually drawn yet.)
+    '''sky'''
     screen.fill(BRIGHT_BLUE)
-    '''ground'''
+
+    '''grass'''
     pygame.draw.rect(screen, BRIGHT_GREEN, [0, 450, 800, 600])
 
     '''house'''
@@ -61,8 +70,37 @@ while not done:
 
     '''sun'''
     pygame.draw.ellipse(screen, YELLOW, [20, 20, 100, 100])
+    
     pygame.draw.line(screen, YELLOW, [0, 60], [30, 60], 3)
     pygame.draw.line(screen, YELLOW, [5, 30], [30, 40], 3)
+    pygame.draw.line(screen, YELLOW, [0, 90], [30, 80], 3)
+    pygame.draw.line(screen, YELLOW, [6, 120], [30, 100], 3)
+    pygame.draw.line(screen, YELLOW, [32, 140], [53, 110], 3)
+    pygame.draw.line(screen, YELLOW, [70, 120], [70, 145], 3)
+    pygame.draw.line(screen, YELLOW, [95, 110], [110, 140], 3)
+    pygame.draw.line(screen, YELLOW, [105, 90], [140, 120], 3)
+    pygame.draw.line(screen, YELLOW, [110, 80], [150, 85], 3)
+    pygame.draw.line(screen, YELLOW, [105, 60], [145, 55], 3)
+    pygame.draw.line(screen, YELLOW, [80, 55], [135, 25], 3)
+    pygame.draw.line(screen, YELLOW, [90, 25], [110, 5], 3)
+    pygame.draw.line(screen, YELLOW, [70, 30], [70, 0], 3)
+    pygame.draw.line(screen, YELLOW, [45, 25], [30, 5], 3)
+
+    '''clouds'''
+    draw_cloud(50, 150)
+    draw_cloud(200, 75)
+    draw_cloud(350, 10)
+    draw_cloud(475, 30)
+    draw_cloud(650, 125)
+
+    '''fence'''
+    y = 380
+    for x in range(5, 800, 30):
+       pygame.draw.polygon(screen, WHITE, [[x+5, y+20], [x+10, y+35],
+                                            [x+10, y+80], [x, y+80],
+                                            [x, y+35]])
+    pygame.draw.line(screen, WHITE, [0, 425], [800, 425], 5)
+    pygame.draw.line(screen, WHITE, [0, 445], [800, 445], 5) 
 
     '''pygame.draw.line(screen, GREEN, [300, 40], [100,500], 5)'''
     '''pygame.draw.ellipse(screen, BLUE, [100, 100, 600, 300])'''
