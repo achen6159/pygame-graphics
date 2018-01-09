1020703cced96048c80a1f3616a5503481a1582a
achen6159 <31477651+achen6159@users.noreply.github.com>
2018-01-09 09:24:40 -0500

    Add files via upload

--- graphics_demo.py
+++ graphics_demo.py
@@ -1,6 +1,7 @@
 # Imports
 import pygame
 import math
+import random
 
 # Initialize game engine
 pygame.init()
@@ -29,7 +30,8 @@ BRIGHT_BLUE = (39, 172, 244)
 PURPLE = (179, 39, 244)
 BRIGHT_GREEN = (70, 244, 39)
 YELLOW = (244, 234, 41)
-    
+BROWN = (99, 63, 9)
+DARK_GREEN = (12, 135, 24)
 
 def draw_cloud(x, y):
     pygame.draw.ellipse(screen, WHITE, [x, y + 20, 40 , 40])
@@ -37,6 +39,26 @@ def draw_cloud(x, y):
     pygame.draw.ellipse(screen, WHITE, [x + 20, y + 10, 25, 25])
     pygame.draw.ellipse(screen, WHITE, [x + 35, y, 50, 50])
     pygame.draw.rect(screen, WHITE, [x + 20, y + 20, 60, 40])
+    
+
+def draw_tree(x, y):
+    pygame.draw.rect(screen, BROWN, [x + 20, y + 20, 50, 200])
+    pygame.draw.ellipse(screen, DARK_GREEN, [x, y - 50, 90, 150])
+
+def draw_house(x, y):
+    pygame.draw.rect(screen, ORANGE, [x, y, 300, 200])
+    pygame.draw.rect(screen, BLACK, [x, y, 300, 200], 3)
+    pygame.draw.polygon(screen, BLACK, [[x - 50, y], [x + 150, y - 100], [x + 350, y]], 5)
+    pygame.draw.polygon(screen, RED, [[x - 50, y], [x + 150, y - 100], [x + 350, y]])
+    pygame.draw.rect(screen, BLACK, [x + 100, y + 50, 100, 150], 5)
+    pygame.draw.rect(screen, PURPLE, [x + 100, 300, 100, 150])
+    pygame.draw.ellipse(screen, BLACK, [x + 115, y + 110, 15, 15])
+
+def draw_flowers(x, y):
+    pygame.draw.rect(screen, DARK_GREEN, [x + 2, y + 10, 5, 20])
+    pygame.draw.ellipse(screen, RED, [x, y + 4, 10, 10])
+
+
     
 # Game loop
 done = False
@@ -60,13 +82,7 @@ while not done:
     pygame.draw.rect(screen, BRIGHT_GREEN, [0, 450, 800, 600])
 
     '''house'''
-    pygame.draw.rect(screen, ORANGE, [300, 250, 300, 200])
-    pygame.draw.rect(screen, BLACK, [300, 250, 300, 200], 3)
-    pygame.draw.polygon(screen, BLACK, [[250, 250], [450, 150], [650, 250]], 5)
-    pygame.draw.polygon(screen, RED, [[250, 250], [450, 150], [650, 250]])
-    pygame.draw.rect(screen, BLACK, [400, 300, 100, 150], 5)
-    pygame.draw.rect(screen, PURPLE, [400, 300, 100, 150])
-    pygame.draw.ellipse(screen, BLACK, [415, 360, 15, 15])
+    draw_house(300, 250)
 
     '''sun'''
     pygame.draw.ellipse(screen, YELLOW, [20, 20, 100, 100])
@@ -87,11 +103,16 @@ while not done:
     pygame.draw.line(screen, YELLOW, [45, 25], [30, 5], 3)
 
     '''clouds'''
-    draw_cloud(50, 150)
+    draw_cloud(150, 75)
     draw_cloud(200, 75)
     draw_cloud(350, 10)
-    draw_cloud(475, 30)
-    draw_cloud(650, 125)
+    draw_cloud(400, 20)
+    draw_cloud(650, 70)
+    draw_cloud(280, 20)
+    draw_cloud(550, 100)
+    draw_cloud(350, 20)
+    draw_cloud(700, 70)
+    draw_cloud(500, 100)
 
     '''fence'''
     y = 380
@@ -100,11 +121,32 @@ while not done:
                                             [x+10, y+80], [x, y+80],
                                             [x, y+35]])
     pygame.draw.line(screen, WHITE, [0, 425], [800, 425], 5)
-    pygame.draw.line(screen, WHITE, [0, 445], [800, 445], 5) 
+    pygame.draw.line(screen, WHITE, [0, 445], [800, 445], 5)
 
-    '''pygame.draw.line(screen, GREEN, [300, 40], [100,500], 5)'''
-    '''pygame.draw.ellipse(screen, BLUE, [100, 100, 600, 300])'''
-    '''pygame.draw.polygon(screen, BLACK, [[200, 200], [50,400], [600, 500]], 10)'''
+
+    '''snow'''
+    snow = []
+    for x in range(200):
+        x = random.randrange(0, 800)
+        y = random.randrange(0, 400)
+        r = random.randrange(1, 5)
+        snow.append([x, y, r, r])
+    for s in snow:
+        pygame.draw.ellipse(screen, WHITE, s)
+
+    '''tree'''
+    draw_tree(80, 300)
+
+    '''flowers'''
+    draw_flowers(200, 500)
+    draw_flowers(600, 500)
+    draw_flowers(50, 450)
+    draw_flowers(500, 475)
+    draw_flowers(700, 450)
+    draw_flowers(400, 450)
+    draw_flowers(300, 550)
+    draw_flowers(150, 450)
+
 
     ''' angles for arcs are measured in radians (a pre-cal topic) '''
     '''pygame.draw.arc(screen, ORANGE, [100, 100, 100, 100], 0, math.pi/2, 1)'''
